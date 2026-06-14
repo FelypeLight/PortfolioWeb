137845f7d8d5687cedeb9bccd966f24fedcf9354
light <z8022224@gmail.com>
2026-06-14 19:14:44 -0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-CwmEJt-Q.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/qualquer/Documentos/Meu Portfolio/portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-nDf8xLP_.js"] }, "/": { filePath: "/home/qualquer/Documentos/Meu Portfolio/portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CUyCiQlA.js"] } }, clientEntry: "/assets/index-nDf8xLP_.js" });
+export {
+  tsrStartManifest
+};
